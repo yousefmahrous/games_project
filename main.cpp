2702d9379250bd6d26d32530769f87d202e783ab
Ahmed-Soliman7 <ahayfa06@gmail.com>
2025-11-26 11:18:55 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -11,15 +11,17 @@
 #include "NumericalTTT.h"
 #include "obstacles.h"
 #include "infinit.h"
+#include "SUS.h"
 
 using namespace std;
 
 void games() {
-    cout << "1. Misere Tic Tac Toe" << endl;
-    cout << "2. 4 x 4 Tic-Tac-Toe" << endl;
-    cout << "3. 3 x 3 Numerical Tic-Tac-Toe" << endl;
-    cout << "4. Obstacles Tic-Tac-Toe" << endl;
-    cout << "5. Infinit Tic-Tac-Toe" << endl;
+    cout << "1. SUS" << endl;
+    cout << "2. Misere Tic Tac Toe" << endl;
+    cout << "3. 4 x 4 Tic-Tac-Toe" << endl;
+    cout << "4. 3 x 3 Numerical Tic-Tac-Toe" << endl;
+    cout << "5. Obstacles Tic-Tac-Toe" << endl;
+    cout << "6. Infinit Tic-Tac-Toe" << endl;
     cout << "0. Exit" << endl;
 }
 
@@ -30,6 +32,24 @@ void game_torun() {
         exit(0);
     }
     else if (choice == '1') {
+        srand(static_cast<unsigned int>(time(0)));
+
+        UI<char>* game_ui = new SUS_UI();
+        Board<char>* sus_board = new SUS_Board();
+        Player<char>** players = game_ui->setup_players();
+
+        GameManager<char> sus_game(sus_board, players, game_ui);
+        sus_game.run();
+
+        // Cleanup
+        delete sus_board;
+        delete game_ui;
+        for (int i = 0; i < 2; ++i) {
+            delete players[i];
+        }
+        delete[] players;
+    }
+    else if (choice == '2') {
         srand(static_cast<unsigned int>(time(0)));
 
         UI<char>* game_ui = new misere_UI();
@@ -49,7 +69,7 @@ void game_torun() {
         }
         delete[] players;
     }
-    else if (choice == '2') {
+    else if (choice == '3') {
         srand(static_cast<unsigned int>(time(0)));
 
         UI<char>* game_ui = new fourinfour_UI();
@@ -69,29 +89,30 @@ void game_torun() {
         }
         delete[] players;
 
-    }else if (choice == '3') {
-     srand(static_cast<unsigned int>(time(nullptr)));
-
-     NumericalTTT_UI* game_ui = new NumericalTTT_UI();
-     NumericalTTT_Board* num_board = new NumericalTTT_Board(3, 3);
-
-     Player<int>** players = game_ui->setup_players();
-
-
-     players[0]->set_board_ptr(num_board);
-     players[1]->set_board_ptr(num_board);
-
-     GameManager<int> game(num_board, players, game_ui);
-     game.run();
-
-   
-     delete num_board;
-     delete players[0];
-     delete players[1];
-     delete[] players;
-     delete game_ui;
     }
     else if (choice == '4') {
+        srand(static_cast<unsigned int>(time(nullptr)));
+
+        NumericalTTT_UI* game_ui = new NumericalTTT_UI();
+        NumericalTTT_Board* num_board = new NumericalTTT_Board(3, 3);
+
+        Player<int>** players = game_ui->setup_players();
+
+
+        players[0]->set_board_ptr(num_board);
+        players[1]->set_board_ptr(num_board);
+
+        GameManager<int> game(num_board, players, game_ui);
+        game.run();
+
+
+        delete num_board;
+        delete players[0];
+        delete players[1];
+        delete[] players;
+        delete game_ui;
+    }
+    else if (choice == '5') {
         srand(static_cast<unsigned int>(time(0)));
 
         UI<char>* game_ui = new obstacles_UI();
@@ -111,7 +132,7 @@ void game_torun() {
         }
         delete[] players;
     }
-    else if (choice == '5') {
+    else if (choice == '6') {
         srand(static_cast<unsigned int>(time(0)));
 
         UI<char>* game_ui = new infinit_UI();
